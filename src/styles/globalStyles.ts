8ff93e21b499c42bfa8860337799bbd5a467345b
r-vilemen <rodrigo.vilemen@hotmail.com>
2023-07-06 12:28:56 -0300

    feat: update estructure

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+
+  a {
+    color: #333;
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyles;
---
+++ src/styles/theme.tsx
@@ -0,0 +1,13 @@
+const theme = {
+  colors: {
+    primary: "#ff0000",
+    secondary: "#00ff00",
+  },
+  fontSizes: {
+    small: "14px",
+    medium: "16px",
+    large: "18px",
+  },
+};
+
+export default theme;
